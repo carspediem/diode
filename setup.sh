7376fff44b5c9261a5c0e5aa87dbc69d3f0d0c02
carspediem <backsdiem@gmail.com>
2025-04-22 20:07:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-#!/bin/bashhttps://github.com/carspediem/diode/blob/main/setup.sh
+#!/bin/bash
+
 echo "📦 Gerekli temel paketler kuruluyor..."
 sudo apt update && sudo apt install -y sudo apt ufw snapd
+sleep 5
+
 echo "🔧 Gerekli portlar açılıyor..."
 sudo ufw allow 22
 sudo ufw allow 38537
@@ -12,13 +15,17 @@ sudo ufw allow 1723
 sudo ufw allow 10000
 sudo ufw allow 8545
 sudo ufw allow 8443
+sleep 5
 sudo ufw reload
+sleep 5
 
 echo "🚀 Sistem güncelleniyor..."
 sudo apt update && sudo apt upgrade -y
+sleep 5
 
 echo "📁 Snapd tekrar kuruluyor..."
 sudo apt install snapd -y
+sleep 5
 
 echo "🛠️ Diode Node kuruluyor..."
 sudo snap install diode-node
@@ -28,5 +35,6 @@ sleep 20
 
 echo "📦 Node bilgileri gösteriliyor..."
 diode info
+sleep 5
 
 echo "İşlem tamamlandı.📬 İletişim: X @dakpluto"
